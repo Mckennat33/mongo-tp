0cfbdb87d50f5d5139526f001743eee8ea510bbc
tom mckenna <thomasmckenna12@gmail.com>
2024-11-20 20:13:06 -0600

    added the find method and filter methods to my model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground') 
+mongoose.connect('mongodb://localhost/playground', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}) 
 .then(() => console.log('connected to mongo db')) 
 .catch(err => console.error("Could not connect to mongo db", err))
 
@@ -19,15 +22,25 @@ const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
     const course = new Course({
-        name: 'Node.js Course', 
+        name: 'Angular Course', 
         author: 'Thomas', 
-        tags: ['node', 'backend'], 
+        tags: ['angular', 'frontend'], 
         isPublished: true
     });
     
     // Saving a document / asynchronous
-    const result = await course.save();
+    const result = await Course.save();
     console.log(result);
 }
 
-createCourse();
+async function getCourses() {
+    const courses = await Course
+        .find({ author: 'Thomas', isPublished: true })
+        .limit(10) 
+        .sort({ name: 1 }) // can sort the document, -1 would sort in a decending order. 
+        .select({ name: 1, tags: 1 }) // select the properties that we want returned.
+    console.log(courses)
+
+}
+
+getCourses();
